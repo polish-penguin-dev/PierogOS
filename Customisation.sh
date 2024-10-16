5dfd18a34262be8942b9c922423fc96dbac02fea
polish-penguin-dev <alexwegrzyn183@gmail.com>
2024-10-16 17:19:21 +0100

    first commit

---
+++ Customisation.sh
@@ -0,0 +1,33 @@
+echo -e "\033[0;36mPierogOS Cubic Customisation Script\033[0m"
+
+echo "
+Contents
+========
+
+1. Plymouth Customisation
+2. Ubiquity Customisation
+3. Background Image Customisation
+4. Neofetch Customisation
+5. Icon Customisation
+"
+
+echo -e "\033[0;32m[INFO]\033[0m Installing Wget..."
+sudo apt install wget -y
+
+#Customise Plymouth
+#echo -e "\033[0;32m[INFO]\033[0m Beginning Plymouth Customisation..."
+
+#Customise Ubiquity
+#echo -e "\033[0;32m[INFO]\033[0m Beginning Ubiquity Customisation..."
+
+#Customise Backgrounds
+echo -e "\033[0;32m[INFO]\033[0m Beginning Background Customisation..."
+mkdir /usr/share/backgrounds/pierogos
+cd /usr/share/backgrounds/pierogos
+
+#Customise Neofetch
+#echo -e "\033[0;32m[INFO]\033[0m Beginning Neofetch Customisation..."
+
+#Customise Icons
+#echo -e "\033[0;32m[INFO]\033[0m Beginning Icon Customisation..."
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# PierógOS
+## PierógOS Is A Free Linux Distribution Based On Zorin, Built With Cubic.
+
+**All Files, Scripts, And Assets Are Hosted Here**
+
+NOT Actively Maintained, But Should Be Fine If You Build It Yourself; Created And Last Built In Oct/Nov 2024
+
+Polish!
+Built By Penguins184 & LOT
