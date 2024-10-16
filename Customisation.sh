b9a00d4e881629b6e2ddd061bda13725f8f7f6e2
polish-penguin-dev <alexwegrzyn183@gmail.com>
2024-10-16 18:19:26 +0100

    FInish Background Customisation

--- Customisation.sh
+++ Customisation.sh
@@ -25,6 +25,25 @@ echo -e "\033[0;32m[INFO]\033[0m Beginning Background Customisation..."
 mkdir /usr/share/backgrounds/pierogos
 cd /usr/share/backgrounds/pierogos
 
+base_url="https://raw.githubusercontent.com/polish-penguin-dev/PierogOS/refs/heads/main/Assets/Backgrounds/PierogOS_BG"
+for i in 1 2 3 4
+do
+    wget "${base_url}${i}.png"
+done
+
+cd /usr/share/gnome-background-properties
+wget https://github.com/polish-penguin-dev/PierogOS/raw/refs/heads/main/Assets/Backgrounds/Wallpapers.xml
+
+override_file="/usr/share/glib-2.0/schemas/pierogos-wallpaper.gschema.override"
+
+echo "[org.gnome.desktop.background]" > $override_file
+echo "picture-uri = 'file:///usr/share/backgrounds/pierogos/PierogOS_BG1.png'" >> $override_file
+echo "picture-options = 'zoom'" >> $override_file
+
+glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+echo -e "\033[0;32m[INFO]\033[0m Completed Background Customisation..."
+
 #Customise Neofetch
 #echo -e "\033[0;32m[INFO]\033[0m Beginning Neofetch Customisation..."
 
